e234f97c2fdb07f9b4141decd186563589c99312
senindu003 <senindudinapura@gmail.com>
2026-01-18 06:45:42 +0530

    allow-credentials=False

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
   CORSMiddleware,
   allow_origins=["*"],
-  allow_credentials=True,
+  allow_credentials=False,
   allow_methods=["*"],
   allow_headers=["*"],
 )
@@ -34,4 +34,4 @@ app.include_router(get_info_router)
 
 if __name__ == "__main__":
   import uvicorn
-  uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+  uvicorn.run(app, host="0.0.0.0", port=8000)
